b9e10f811b6a631d88284946ba1e51a5e16b13e3
Jovan Stojkoski <jovanstjk2@hotmail.com>
2026-09-19 19:22:05 +0200

    update swagger

--- src/Claims.API/Program.cs
+++ src/Claims.API/Program.cs
@@ -19,21 +19,14 @@ await builder.Services.AddInfrastructureAsync(
     builder.Environment);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Claims API",
-        Version = "v1"
-    });
-});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI(options =>
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Claims API v1"));
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
