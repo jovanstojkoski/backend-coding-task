b3b50c12d90597a5931f244c8b10bdc32cfc28b5
Jovan Stojkoski <jovanstjk2@hotmail.com>
2026-09-19 19:10:43 +0200

    add missing file

---
+++ src/Claims.Infrastructure/Data/Queues/AuditQueueOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Claims.Infrastructure.Data.Queues
+{
+    public sealed class AuditQueueOptions
+    {
+        public const string SectionName = "AuditQueue";
+
+        public int Capacity { get; init; } = 1_000;
+    }
+}
